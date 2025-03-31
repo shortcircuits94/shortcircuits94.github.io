cf4fe065aa3137b20b2669321edf258582db40f7
Rebecca Scott <62228611+shortcircuits94@users.noreply.github.com>
2025-03-31 14:36:19 -0400

    created bio component

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.scss";
+import Bio from "../../components/Bio/Bio";
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Bio />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/Bio/Bio.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "../Bio/Bio.scss";
+import Rebecca from "../../assets/rebecca-scott.jpg";
+
+const Bio = () => {
+  return (
+    <footer className="bio">
+      <section className="bio__content">
+        <div className="bio__header">
+          <img
+            className="bio__headshot"
+            src={Rebecca}
+            alt="Rebecca Scott Headshot"
+          />
+          <h3 className="bio__title">Meet Rebecca Scott</h3>
+        </div>
+        <div className="bio__info">
+          <h4>Hi!</h4>
+          <p>I'm so happy you're here!</p>
+          <br></br>
+          <p>
+            I'm a software engineer with a background in design and digital
+            marketing, bringing a unique blend of creativity and technical
+            skill. I focus on building user-centered applications that are
+            intuitive, functional, and visually polished. With a sharp design
+            eye and a developer’s mindset, I love turning ideas into seamless
+            digital experiences.
+          </p>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Bio;
