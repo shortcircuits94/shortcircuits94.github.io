f90b703f12bd77a18ca9708a21df7ae94dfb578a
Rebecca Scott <62228611+shortcircuits94@users.noreply.github.com>
2025-03-31 12:57:44 -0400

    developed header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Header from "./components/Header";
+
 import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Header.scss";
+import Logo from "../assets/logo.png";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <Link to="/" onClick={handleLinkClick}>
+          <img src={Logo} alt="Rebecca Scott Logo" />
+        </Link>
+      </div>
+
+      {/* Mobile Hamburger Menu */}
+      <div
+        className={`header__menu-toggle ${isMenuOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <span className="header__menu-icon"></span>
+      </div>
+
+      {/* Navigation Links */}
+      <nav className={`header__nav ${isMenuOpen ? "open" : ""}`}>
+        <>
+          <Link to="/work" className="header__link" onClick={handleLinkClick}>
+            Work
+          </Link>
+          <Link
+            to="/projects"
+            className="header__link"
+            onClick={handleLinkClick}
+          >
+            Projects
+          </Link>
+        </>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
